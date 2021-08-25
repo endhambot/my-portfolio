a1a69b8250d4025005b290c7955a1fe242c03dd8
endhambot <endhambot@gmail.com>
2021-08-25 20:48:19 +0530

    jah

---
+++ src/portfolio.js
@@ -0,0 +1,498 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello.",
+  title2: "Thulina",
+  logo_name: "Thulina.p()",
+  nickname: "Endham / Endhambot",
+  full_name: "Thulina Perera",
+  subTitle:
+    "Full Stack Developer, Tech Learning Enthusiast. Always learning.",
+  resumeLink:
+    "",
+  mail: "mailto:endhamfc@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/endhambot",
+  linkedin: "https://www.facebook.com/thulina.perera.585",
+  gmail: "mailto:endhamfc@gmail.com",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Developing mobile applications using Flutter and native Android",
+        "⚡ Creating application backend in Node, Express & Flask",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        
+        {
+          skillName: "GraphQL",
+          fontAwesomeClassname: "simple-icons:graphql",
+          style: {
+            color: "#DE33A6",
+          },
+        },
+        {
+          skillName: "Android",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+        
+        {
+          skillName: "Go",
+          fontAwesomeClassname: "simple-icons:go",
+          style: {
+            color: "#00A7D0",
+          },
+        },
+        {
+          skillName: "Visual Basic",
+          fontAwesomeClassname: "simple-icons:dot-net",
+          style: {
+            color: "#029FCE",
+          },
+        },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+        
+        
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        
+        
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Deploying deep learning models on cloud to use on mobile devices",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "GCP",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+      ],
+    },
+    
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Ananda College Colombo 10",
+      subtitle: "study commerce as A/L subject",
+      logo_path: "sal.png",
+      alt_name: "A/C",
+      duration: "2010 - Present",
+      descriptions: [
+        "⚡ I have studied core subjects like ICT, Maths, Science, etc.",
+        "⚡ I have also completed various online competitions like BITS for Web, Mobile App Development, etc.",
+        "⚡ I have implemented several projects based on what I've leart at my school. ",
+      ],
+      website_link: "https://www.acicts.lk/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "BITS 2020",
+      subtitle: "Overall 4rth place",
+      logo_path: "deeplearning_ai_logo.png",
+      certificate_link:
+        "https://acicts.github.io/BITS20/leaderboard.html",
+      alt_name: "BITS2020",
+      color_code: "#47A048",
+    },
+    {
+      title: "BITS 2020",
+      subtitle: "1st in coding tasks",
+      logo_path: "deeplearning_ai_logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/18d3xmkaA_t09jWTc8v8-XCWutB0czk5l/view?usp=sharing",
+      alt_name: "BITS 2020",
+      color_code: "#F6B808",
+    },
+    {
+      title: "Google Code-In Student",
+      subtitle: "2018-2019",
+      logo_path: "google_logo.png",
+      alt_name: "Google",
+      color_code: "#1e70c1",
+    },
+    
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work",
+      experiences: [
+        {
+          title: "Full Stack Developer",
+          company: "Mental Health Foundation",
+          company_url: "https://www.mhflk.ga/",
+          logo_path: "quicko.png",
+          duration: "2020 - Present",
+          location: "Work form home",
+          description:
+            "I created the whole website and all the pages with it.it's a simple one page lms page for the students of the mhflk.",
+          color: "#2962FF",
+        },
+        {
+          title: "Full Stack Developer",
+          company: "ACFC",
+          company_url: "https://endhambot.github.io/anandafc.github.io/",
+          logo_path: "quicko1.jpg",
+          duration: "2019 - Present",
+          location: "Ananda College - Colombo 10 ",
+          description:
+            "created this website for the football team of our school which is Ananda College Football Club aka ACFC.",
+          color: "#2962FF",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "ACICTS",
+          company: "Ananda College ICT Society",
+          company_url: "https://www.acicts.lk/",
+          logo_path: "google_logo1.png",
+          duration: "2018 - Present",
+          location: " ",
+          description:
+            "ACICTS is the official ICT society of my school which is Ananda College.I have actively participated in lot of workshops,competitions as a member",
+          color: "#4285F4",
+        },
+        {
+          title: "BITS 2020 & 2018",
+          company: "BITS.ACICTS",
+          company_url: "https://bits.acicts.lk/",
+          logo_path: "deeplearning_ai_logo.png",
+          duration: "2018 - 2020",
+          location: " ",
+          description:
+            "BITS is The First-Ever Task-Based Intra-School ICT competition, organized by the Ananda College ICT Society .",
+          color: "#D83B01",
+        },
+        {
+          title: "GitHub Student Developer",
+          company: "Github",
+          company_url: "https://github.com/",
+          logo_path: "codeuino.png",
+          duration: "2018 - Present",
+          location: " ",
+          description: "Contribute to Open Source Community and Open Source Project.",
+          color: "#D83B01",
+        },
+        
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "hrishi2.png",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Medium.",
+    link: "https://medium.com/@hrishipatel99",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "First Portfolio",
+      url: "https://mhflk.github.io/endham.github.io/",
+      description: "(This) A clean and full customizable portfolio template.And also this my first portfolio website",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "Simple Ping-Pong game",
+      url: "https://github.com/endhambot/ponggame",
+      description:
+        "This is a simple ping-pong game made with python for one of tasks which were in BITS 2020 .",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Official MHFLK website",
+      url: "https://www.mhflk.ga/",
+      description:
+        "This is the official website of the Mental Health Foundation in Sri Lanka .",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-java",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+    {
+      id: "12",
+      name: "Snake Game (classic)",
+      url: "https://github.com/endhambot/jsgame",
+      description:
+        "This is a classic sanke eat egg game made using Js. This project was build for one of advance task in BITS 2020.",
+      languages: [
+        {
+          name: "JS",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+    {
+      id: "13",
+      name: "ACFC Website",
+      url: "https://endhambot.github.io/anandafc.github.io/",
+      description:
+        "I made this website for the ACFC aka Ananda College Football Club",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Tensorflow",
+          iconifyClass: "logos-tensorflow",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "Simple Chat-Bot",
+      url: "https://github.com/endhambot/easychatbot",
+      description:
+        "I programed this chat-bot using Python and i created this for one of advanced tak which was in BITS 2020.",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
